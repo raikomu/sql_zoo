65357bcf869455101cd54be05e3198d7cf18919b
Cypher <raiko4238@gmail.com>
2017-11-07 21:45:31 +0200

    add lesson 09
    Skipped question 10

---
+++ 09_self_join.sql
@@ -0,0 +1,56 @@
+# 1. How many stops are in the database. 
+SELECT COUNT(*)
+  FROM stops
+
+# 2. Find the id value for the stop 'Craiglockhart' 
+SELECT id
+  FROM stops
+ WHERE name = 'Craiglockhart'
+
+# 3. Give the id and the name for the stops on the '4' 'LRT' service. 
+SELECT id, name
+  FROM stops JOIN route
+    ON id = stop
+ WHERE num = '4'
+   AND company = 'LRT'
+
+# 4. Add a HAVING clause to restrict the output to these two routes. 
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) = 2
+
+# 5. Change the query so that it shows the services from Craiglockhart to London Road. 
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop = 149
+
+# 6. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown.
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND stopb.name = 'London Road'
+
+# 7. Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith') 
+SELECT a.company, a.num 
+  FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+ WHERE a.stop = 115 AND b.stop = 137
+ GROUP BY a.company, a.num
+
+# 8. Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross' 
+SELECT a.company, a.num
+  FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+  JOIN stops stopa ON stopa.id = a.stop
+  JOIN stops stopb ON stopb.id = b.stop
+ WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+ GROUP BY a.company, a.num
+
+# 9. Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus,
+#    including 'Craiglockhart' itself, offered by the LRT company.
+SELECT stopb.name, a.company, a.num
+  FROM route a JOIN route b ON a.company = b.company AND a.num = b.num
+  JOIN stops stopa ON a.stop = stopa.id JOIN stops stopb ON b.stop = stopb.id
+ WHERE a.company = 'LRT' AND stopa.name = 'Craiglockhart'
